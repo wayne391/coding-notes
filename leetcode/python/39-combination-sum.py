e50d6fc592ea497239da794f71ee1808627c75be
wayne391 <s101062219@gmail.com>
2019-06-11 14:38:37 +0800

    update 39

--- leetcode/python/39-combination-sum.py
+++ leetcode/python/39-combination-sum.py
@@ -1,3 +1,31 @@
+'''
+Fast solution
+https://leetcode.com/problems/combination-sum/discuss/304571/Python-DFS-beats-99.76-(52ms-13.4-MB)
+'''
+def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        candidates.sort()
+        res = []
+        
+        def dfs(t, ind, path):
+            if t == 0:
+                res.append(path)
+                return
+            for i in range(ind, n):
+                # 如果不夠湊則break
+                # 因為有sort所以剩下不考慮
+                # 沒有sort會慢不少
+                if candidates[i] > t:
+                    break
+                dfs(t-candidates[i], i, path+[candidates[i]])
+                
+        dfs(target, 0, [])
+        return res
+
+'''
+My original version, much slower
+'''
+
 class Solution:
     def dfs(self, cur_sum, start, path):
         if cur_sum == self.target:
