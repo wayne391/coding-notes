45ad6626a8f86cb7a4fd44d9310284db3c853f84
wayne391 <s101062219@gmail.com>
2019-06-22 00:30:30 +0800

    add 322

---
+++ leetcode/python/322-coin-change.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if not coins:
+            return -1
+        
+        table = [None] * amount + [0]
+    
+        for i in range(amount-1, -1, -1):
+            cand = []
+            for step in coins:
+                tmp = i + step
+                if tmp <= amount and table[tmp] is not None:
+                    cand.append(table[tmp])
+            table[i] = min(cand) + 1  if cand else None
+        return -1 if table[0] == None else table[0]
